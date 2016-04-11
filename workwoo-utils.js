59466a3c2cd2ae07889409c0eee14d27462281b1
Ryan Mangroo <ryan.mangroo@servicenow.com>
2016-04-11 12:49:05 -0700

    Added validator utility

--- workwoo-utils.js
+++ workwoo-utils.js
@@ -3,6 +3,7 @@ exports.cache = require('./lib/cache');
 exports.logger = require('./lib/logger');
 exports.mailer = require('./lib/mailer');
 exports.utility = require('./lib/utility');
+exports.validator = require('./lib/validator');
 
 //Common Models
 exports.notificationTemplate = require('./lib/models/notificationTemplate');
