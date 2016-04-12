63d22689910e6ab98924e60e12bf1577660305a9
Ryan Mangroo <ryan.mangroo@servicenow.com>
2016-04-12 14:57:53 -0700

    Move validation work

--- tests/endPointTester.js
+++ tests/endPointTester.js
@@ -7,18 +7,10 @@ var request = require('request');
 var j = request.jar();
 var btoa = require('btoa');
 
-var testFinished = false;
-
 run();
 
-function end() {
-	setTimeout(function() { 
-		if (testFinished) { 
-			process.exit();
-		} else {
-			end();
-		}
-	}, 2000);
+function finishTest() {
+	process.exit();
 }
 
 function run() {
@@ -32,10 +24,9 @@ function run() {
 			case 'auth' : testAuthentication(); break;
 			case 'fp' : testForgotPassword(); break;
 			case 'rp' : testResetPassword(); break;
+			case 'ua' : testUpdateMyAccount(); break;
 			default : console.log('Invalid test type: ' + testType); process.exit();break;
 		}
-
-		end();
 	}
 
 }
@@ -51,10 +42,10 @@ function testAuthentication() {
 
 	console.log("Logging in with " + emailAddress + ":" + password);
 
-	logIn(emailAddress, password);
+	logIn(emailAddress, password, logOut);
 }
 
-function logIn(emailAddress, password) {
+function logIn(emailAddress, password, callback) {
 	request({
 		url: cfg.auth.url + "/login",
 		method: "POST",
@@ -68,16 +59,15 @@ function logIn(emailAddress, password) {
 		var cookie_string = j.getCookieString(cfg.auth.url + '/login'); 
 		if (cookie_string) {
 			console.log(cookie_string);
-			console.log("Logging out with " + emailAddress + ":" + password);
-			logOut();	
+			callback(emailAddress);
 		} else {
-			testFinished = true;
+			finishTest();
 		}
 		
 	});
 }
 
-function logOut() {
+function logOut(emailAddress) {
 	request({
 		url: cfg.platform.url + "logout",
 		method: "GET",
@@ -85,10 +75,9 @@ function logOut() {
 		jar: j
 	}, function(error, response, body) {
 		//console.log("Response to logout: " + body);
-		process.exit();
-		testFinished = true;
+		finishTest();
 	});
-}
+};
 
 function testForgotPassword() {
 	if (process.argv.length < 4) {
@@ -100,10 +89,10 @@ function testForgotPassword() {
 
 	console.log("Executing forgot password with " + emailAddress);
 
-	forgotPassword(emailAddress);
+	forgotPassword(emailAddress, finishTest);
 }
 
-function forgotPassword(emailAddress) {
+function forgotPassword(emailAddress, callback) {
 	var data = {};
 	data.emailAddress = emailAddress;
 
@@ -118,9 +107,9 @@ function forgotPassword(emailAddress) {
 		console.log("Response to forgot password: ");
 		utility.logObject(body);
 		console.log("Check your email for token if successful");
-		testFinished = true;
+		callback();
 	});
-}
+};
 
 function testResetPassword() {
 	if (process.argv.length < 5) {
@@ -133,10 +122,10 @@ function testResetPassword() {
 
 	console.log("Executing reset password with " + newPassword + ":" + token);
 
-	resetPassword(newPassword, token);
+	resetPassword(newPassword, token, finishTest);
 }
 
-function resetPassword(newPassword, token) {
+function resetPassword(newPassword, token, callback) {
 	var data = {};
 	data.newPassword = newPassword;
 	data.token = token;
@@ -151,8 +140,62 @@ function resetPassword(newPassword, token) {
 	}, function(error, response, body) {
 		console.log("Response to reset password: " + body);
 		utility.logObject(body);
-		testFinished = true;
+		callback();
 	});
+};
+
+function testUpdateMyAccount() {
+	if (process.argv.length < 5) {
+		console.log('Email Address and Password required: node endPointTester.js ua [emailAddress] [password]');
+		process.exit();
+	}
+
+	var emailAddress = process.argv[3];
+	var password = process.argv[4];
+
+	console.log("Logging in with " + emailAddress + ":" + password);
+
+	logIn(emailAddress, password, updateMyAccount);
 }
+
+function updateMyAccount(emailAddress) {
+	console.log("Update account for " + emailAddress);
+	
+	var data = {};
+	data.user = {};
+	data.user.firstName = 'endPoint';
+	data.user.lastName = 'Tester';
+	data.user.phone = '(111)-123-4567';
+
+	data.org = {};
+	data.org.name = 'endPointTester Corp.';
+	data.org.phone = '(111)-123-4567';
+
+	request({
+		url: cfg.platform.url + "updateMyAccount",
+		method: "POST",
+		json: true,
+		body: data,
+		withCredentials: true,
+		jar: j
+	}, function(error, response, body) {
+		console.log("Response to update account: " + body);
+		utility.logObject(body);
+		logOut();
+	});
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
